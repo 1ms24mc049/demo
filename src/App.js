e37895140850d0c47d7765e4c87022c6801a0f6c
1ms24mc049 <1ms24mc049@gmail.com>
2026-01-28 11:20:57 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Devops lab </h1>
+      <p>Implementing a fully automated CI/CD pipeline</p>
+    </div>
+  );
+}
+
+export default App;
